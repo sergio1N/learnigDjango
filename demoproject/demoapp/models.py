b5dc558f8aaabfffb2c23c21b94469572bb00e11
sergio1N <sergionaranjo378@gmail.com>
2025-07-26 15:37:22 -0500

    Add initial Menu model and migrations with lastprice field

--- demoproject/demoapp/models.py
+++ demoproject/demoapp/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Menu(models.Model):
+    name=models.CharField(max_length=100)
+    cuisine=models.CharField(max_length=100)
+    price=models.IntegerField()
+    lastprice=models.IntegerField(default=0)
+
+
+    def __str__(self):
+        return self.name  + ' - ' + self.cuisine + ' - $' + str(self.price)    
