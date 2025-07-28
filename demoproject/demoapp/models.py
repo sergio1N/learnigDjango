ee1eb5c02e366e1c40b2bf83eb76429e1d8da038
sergio1N <sergionaranjo378@gmail.com>
2025-07-27 21:52:42 -0500

    use foreign keys

--- demoproject/demoapp/models.py
+++ demoproject/demoapp/models.py
@@ -1,11 +1,24 @@
 from django.db import models
 
+
+# practica con foreing key:
+class Menu_category(models.Model):
+    menu_category_name = models.CharField(max_length=200)
+
+
+
 class Menu(models.Model):
     name=models.CharField(max_length=100)
     cuisine=models.CharField(max_length=100)
-    price=models.IntegerField()
-    lastprice=models.IntegerField(default=0)
+    price=models.IntegerField(null=False)
+    category_id=models.ForeignKey(
+        Menu_category
+        ,on_delete=models.PROTECT
+        # , default=0
+        ,null=True
+        ,blank=True
+        )
 
 
-    def __str__(self):
-        return self.name  + ' - ' + self.cuisine + ' - $' + str(self.price)    
+
+ 
